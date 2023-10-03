44f59f55b085e08709e6bfc55000b62aefdeb5dc
Ilya Shevchenko <6BLK@mail.ru>
2023-10-03 18:58:07 +0300

    Two methods were added

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void  CreateArray(string[]array)
+{
+    Console.WriteLine("Please input 5 string elements of array:");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Please input {i + 1}  element of the array: ");
+        array[i] = Console.ReadLine();
+    }
+    Console.WriteLine();
+}
+
+void PrintArray(string[]array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");        
+    }
+}
+/*
+SortArray(string[])
+{
+
+}
+*/
+
+string[]array = new string[5];
+CreateArray(array);
+PrintArray(array);
